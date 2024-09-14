65be6d543f2edc9c67ee49b9c9cc66b1e03d721e
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-09-14 17:01:54 -0400

    chore: run migration

---
+++ .migrations/0003_fancy_scarecrow.sql
@@ -0,0 +1,15 @@
+ALTER TABLE "goal_completions" DROP CONSTRAINT "goal_completions_goal_id_goals_id_fk";
+--> statement-breakpoint
+ALTER TABLE "goals" DROP CONSTRAINT "goals_user_id_users_id_fk";
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "goal_completions" ADD CONSTRAINT "goal_completions_goal_id_goals_id_fk" FOREIGN KEY ("goal_id") REFERENCES "public"."goals"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "goals" ADD CONSTRAINT "goals_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
